6e4679888675365f0e156be347f34ad59c2c3de2
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2023-08-21 20:28:45 +0700

    create get IP Address function

--- fetchdata.js
+++ fetchdata.js
@@ -1,9 +1,8 @@
 const fetchData = async (ipAddress) => {
-    // const apiKey = "at_t2mWuETqEWMV2FR6xOCl94xDnOHvU"
-    // const response = await fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${apiKey}&ipAddress=${ipAddress}`)
-    // const data = await response.json();
-    // console.log(data.location.city)
-    console.log(ipAddress)
+    const apiKey = "at_t2mWuETqEWMV2FR6xOCl94xDnOHvU"
+    const response = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=${ipAddress}`)
+    const data = await response.json();
+    return data
 }
 
 export default fetchData;
--- main.js
+++ main.js
@@ -2,16 +2,29 @@ import './style.css'
 import { setupMap } from './showmap'
 import fetchData from './fetchdata'
 
+let data = [];
+const outputIpAddress = document.getElementById("output-ip-address")
+const outputLocation = document.getElementById("output-location")
+const outputTimezone = document.getElementById("output-timezone")
+const outputIsp = document.getElementById("output-isp")
+
 setupMap()
 document.addEventListener('DOMContentLoaded', () => {
 
 })
 const formInput = document.getElementById('form-input')
-formInput.addEventListener("submit", (e) => {
+formInput.addEventListener("submit", async (e) => {
   e.preventDefault()
 
   const inputIpAddres = document.getElementById('input-ipaddress').value;
-  fetchData(inputIpAddres)
+  const fetchDataa = await fetchData(inputIpAddres)
+  data.push(fetchDataa);
+
+  console.log(data[0])
+  outputIpAddress.innerText = data[0].ip
+  outputLocation.innerText = data[0].location.country + ", " + data[0].location.region
+  outputTimezone.innerText = data[0].location.timezone
+  outputIsp.innerText = data[0].isp
 })
 // document.querySelector('#app').innerHTML = `
 //   <div>
